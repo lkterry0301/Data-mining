d5b65e9aaf8c70b378dc7ec283aeab4d01674e53
jtronlabs <jtronlabs@gmail.com>
2015-09-06 12:45:44 -0400

    Successfully parse every data file in the data_files folder using the BeautifulSoup python library

---
+++ lab1/lab1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import nltk #natural language toolkit
+from bs4 import BeautifulSoup #xml/html parser, will be used for sgml data files
+
+def transform_document_text_for_parsing(all_document_text):
+    #Parsers assume the document has a wrapper. The provided data files do not. So add one in after the Doctype declaration and close it at the end of the file.
+    oldBeginning = "<!DOCTYPE lewis SYSTEM \"lewis.dtd\">"
+    newBeginning = "<!DOCTYPE lewis SYSTEM \"lewis.dtd\"><TOTAL_DOCUMENT_WRAPPER_TAG_FOR_PARSING>" #add the tag after DocType
+    new_doc_text = all_document_text.replace(oldBeginning,newBeginning,1)#replace the initial (and only) docType
+    new_doc_text = new_doc_text + "</TOTAL_DOCUMENT_WRAPPER_TAG_FOR_PARSING>"#close new tag
+    return new_doc_text
+
+def get_parsed_document_tree(data_file):
+    #Use BeautifulSoup Library to create a Parse Tree out of the text
+    all_document_text =  data_file.read()
+    new_doc_text = transform_document_text_for_parsing(all_document_text)
+    return BeautifulSoup(new_doc_text,'xml')
+
+def main():
+    visited = False
+    for filename in os.listdir(os.getcwd()+"/data_files"):
+        current_data_file = open("data_files/"+filename, "r")
+        
+        document_tree = get_parsed_document_tree(current_data_file)
+        
+        current_data_file.close()
+
+#calls the main() function
+if __name__ == "__main__":
+    main()
