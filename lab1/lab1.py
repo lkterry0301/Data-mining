395905a1e4bbacdf1cf3daa5d04fc8e266afefe1
jtronlabs <jtronlabs@gmail.com>
2015-09-06 20:43:15 -0400

    Implemented frequency based method of trimming words. Improved efficiency of removing stop words and stemming, while simultaneously made it run a lot longer by removing punctuation. Implemented a data structure for keeping track of word counts across all the documents (this needs to be changed to a document-by-document basis). Current unique, interesting word count is ~550 (down from ~70k without processing).

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import string
 import nltk #natural language toolkit
 from nltk import stem
 from bs4 import BeautifulSoup #xml/html parser, will be used for sgml data files
@@ -23,14 +24,22 @@ def get_parsed_document_tree(data_file):
 
 def get_word_list_and_reduce(text):
     words = nltk.word_tokenize(text.lower())
-    
-    #remove stop words via a set difference
-    words = list(set(words) - set(nltk.corpus.stopwords.words('english')) )
-    
-    #stem evey word
     stemmer = stem.porter.PorterStemmer() #porter can be switched with lancaster or snowball for different stemming variants
-    for i in range(0,len(words)):
-        words[i] = stemmer.stem(words[i])
+    remove_punctuation_map = dict((ord(char), None) for char in string.punctuation) #map string.punctuation values into Unicode. Use this as a mapping function to delete punctuation
+    
+    #iterate through the words and check for special cases
+    for i in range(len(words)-1,-1,-1):
+        try:
+            #remove integers, floats, or any kind of number
+            float(words[i])
+            del words[i]
+        except ValueError:#not a number, check other cases and perform necessary translations (like stemming) on the word
+            words[i] = stemmer.stem(words[i])
+            
+            word = words[i].translate(remove_punctuation_map)#remove any punctuation characters and save to temp variable. This will ensure strings like "...","'s","--",etc are removed but strings like "O'Reilly" are preserved with punctuation
+            #After removing punctuation, "..." maps to "", "'s" maps to "s", and "n't" maps to "nt". Remove these cases and any other 0, 1, or 2 character cases, of which there are no viable English words (other than 'I' and 'A', which are not interesting here).
+            if len(word) <= 2 or word in nltk.corpus.stopwords.words('english'):
+                del words[i]
     
     return words
 
@@ -45,7 +54,34 @@ def get_body_words(reuter):
     
     return get_word_list_and_reduce(body_text)
 
+def count_words(word_list,dictionary):
+    for word in word_list:
+        if dictionary.get(word) == None:
+            dictionary[word] = 1
+        else:
+            dictionary[word] += 1
+    
+def trim_words_based_upon_frequency(dictionary):
+    max_val = max(dictionary.values())
+    threshold = .01
+    
+    for key in dictionary.keys():
+        #remove extremely frequent words
+        if dictionary[key] > (max_val - max_val*threshold):
+            del dictionary[key]
+            
+        #remove extremely infrequent words
+        elif dictionary[key] < (max_val*threshold):
+            del dictionary[key]
+
+def print_data(dictionary):
+    for key,value in dictionary.iteritems():
+        print key+" "+str(value)
+
+
 def main():
+    transaction_data = {'class_label': dict([]),'body':dict([])}
+    
     for filename in os.listdir(os.getcwd()+"/../data_files"):
         current_data_file = open("../data_files/"+filename, "r")
         sgml_tree = get_parsed_document_tree(current_data_file)       
@@ -58,8 +94,20 @@ def main():
             
             #parse body into relevant word tokens
             body_words = get_body_words(reuter)
+            count_words(body_words,transaction_data['body'])
         
         current_data_file.close()
+    
+    print "Before frequency based trimming "
+    print "There are "+str(len(transaction_data['body']))+" unique words"
+    print "The minimum is: "+str(min(transaction_data['body'].values()) )
+    print "The maximum is: "+str(max(transaction_data['body'].values()) )
+    trim_words_based_upon_frequency(transaction_data['body'])
+    print "After frequency based trimming "
+    print "There are "+str(len(transaction_data['body']))+" unique words"
+    print "The minimum is: "+str(min(transaction_data['body'].values()) )
+    print "The maximum is: "+str(max(transaction_data['body'].values()) )
+    print_data(transaction_data['body'])
 
 #calls the main() function
 if __name__ == "__main__":
