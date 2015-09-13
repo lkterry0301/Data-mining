8628d96323bf1d116cb530d1f9c23724408971fc
jtronlabs <jtronlabs@gmail.com>
2015-09-13 12:09:14 -0400

    changed term frequency filtering for document frequency filtering. Now terms that appear in >99% of documents or <1% of documents are filtered out, and it filters much more than term frequency

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -11,6 +11,7 @@ from nltk.corpus import stopwords
 from bs4 import BeautifulSoup #xml/html parser, will be used for sgml data files
 
 #global variables
+remove_threshold_for_term_freq = 20
 stemmer = stem.porter.PorterStemmer() #porter can be switched with lancaster or snowball for different stemming variants
 cached_stop_words = stopwords.words("english") #caching stop words speeds it up a lot
 cached_punctuation = string.punctuation
@@ -37,27 +38,53 @@ def should_filter_out_word(word):
         else:
            return False
 
+def increment_hash(dictionary,key):
+    if key in dictionary:
+        dictionary[key] += 1
+    else:
+        dictionary[key] = 1
+    
 def get_reduced_words_with_their_counts(text,overall_word_counts):
     words = nltk.word_tokenize(text.lower())
     words_with_count = dict([]);
     
     #iterate through the words and add to the word count if needed
-    for word in words:
-        if not should_filter_out_word(word):
-            stemmed_word = stemmer.stem(word)
-            
-            if stemmed_word in words_with_count:
-                words_with_count[stemmed_word] += 1
-            else:
-                words_with_count[stemmed_word] = 1
-            
-            if stemmed_word in overall_word_counts:
-                overall_word_counts[stemmed_word] += 1
-            else:
-                overall_word_counts[stemmed_word] = 1                
-            
+    for i in range(len(words)-1, -1,-1):
+        if not should_filter_out_word(words[i]):
+            stemmed_word = stemmer.stem(words[i])
+            increment_hash(words_with_count,stemmed_word)
+            increment_hash(overall_word_counts,stemmed_word)
     
     return words_with_count
+
+def document_count_of_all_words(data_matrix):
+    document_count = dict([])#all unique words
+    words = data_matrix[0] 
+    for i in range(1,len(data_matrix)):
+        for word in words:
+            if word in data_matrix[i][1]:
+                increment_hash(document_count,word)
+    return document_count
+
+def document_frequency_filtering(data_matrix):
+    print "before document freq filter "+str(len(data_matrix[0]))
+    doc_counts = document_count_of_all_words(data_matrix)
+    words_to_remove = set()
+    upper_remove_threshold_for_document_freq = (len(data_matrix) - 1) * .99
+    lower_remove_threshold_for_document_freq = (len(data_matrix) - 1) * .01
+    print "upper"+str(upper_remove_threshold_for_document_freq)
+    print "lower"+str(lower_remove_threshold_for_document_freq)
+    print "total"+str(len(data_matrix) - 1)
+
+
+    for key in doc_counts.keys():
+        if doc_counts[key] > upper_remove_threshold_for_document_freq:
+            words_to_remove.add(key)
+        elif doc_counts[key] < lower_remove_threshold_for_document_freq:
+            words_to_remove.add(key)
+    
+    remove_words_from_data_matrix(words_to_remove,data_matrix)
+    print "after document freq filter "+str(len(data_matrix[0]))
 
 def get_and_count_body_words(reuter,overall_word_counts):
     text_tag = reuter.find("text")
@@ -92,18 +119,23 @@ def get_class_label(reuter):
     return class_label
 
 def term_frequency_filtering(data_matrix,overall_word_counts):
-    print "before term freq "+str(len(overall_word_counts))
+    print "before term freq filter "+str(len(data_matrix[0]))
+    words_to_remove = set()
+    
     for key in overall_word_counts.keys():
-        if overall_word_counts[key] < 20:
-            remove_word_from_data_matrix(key,data_matrix)
+        if overall_word_counts[key] < remove_threshold_for_term_freq:
+            words_to_remove.add(key)
             del overall_word_counts[key]
-    print "after term freq "+str(len(overall_word_counts))
+    
+    remove_words_from_data_matrix(words_to_remove,data_matrix)
+    print "after term freq filter "+str(len(data_matrix[0]))
 
-def remove_word_from_data_matrix(word,data_matrix):
-    data_matrix[0].remove(word)
-    for i in range(1,len(data_matrix)): #iterate over all the documents in the data matrix
-        if word in data_matrix[i][1]:
-            del data_matrix[i][1][word]
+def remove_words_from_data_matrix(words,data_matrix):
+    data_matrix[0] = data_matrix[0] - words
+    for i in range(1,len(data_matrix)):
+        for word in words:
+            if word in data_matrix[i][1]:
+                del data_matrix[i][1][word]
 
 """
 The data matrix format is as follows:
@@ -129,7 +161,8 @@ def get_data_matrix(directory_with_files):
         
         current_data_file.close()
     
-    term_frequency_filtering(data_matrix,overall_word_counts)
+    document_frequency_filtering(data_matrix)
+    #term_frequency_filtering(data_matrix,overall_word_counts)
     
     return data_matrix
 
