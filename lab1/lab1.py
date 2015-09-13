44e22957a6ce2e45577fab591db7847927ec6a7c
jtronlabs <jtronlabs@gmail.com>
2015-09-12 22:01:09 -0400

    changed functions to create a data matrix instead of transaction vectors. Re-added in filtering based on term/word frequency.

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -4,6 +4,7 @@ import sys
 import os
 import time
 import string
+import math
 import nltk #natural language toolkit
 from nltk import stem
 from nltk.corpus import stopwords
@@ -14,7 +15,6 @@ stemmer = stem.porter.PorterStemmer() #porter can be switched with lancaster or 
 cached_stop_words = stopwords.words("english") #caching stop words speeds it up a lot
 cached_punctuation = string.punctuation
 remove_punctuation_map = dict((ord(char), None) for char in cached_punctuation) #map string.punctuation values into Unicode. Use this as a mapping function to delete punctuation
-frequency_trimming_threshold = .01
 
 def get_parsed_document_tree(data_file):
     #Use the BeautifulSoup Library to create a Parse Tree out of the text
@@ -37,106 +37,117 @@ def should_filter_out_word(word):
         else:
            return False
 
-def get_word_list_and_reduce(text):
+def get_reduced_words_with_their_counts(text,overall_word_counts):
     words = nltk.word_tokenize(text.lower())
+    words_with_count = dict([]);
     
-    #iterate through the words and check for special cases
-    for i in range(len(words)-1,-1,-1):
-        if should_filter_out_word(words[i]):
-            del words[i]
-        else:
-            words[i] = stemmer.stem(words[i])
+    #iterate through the words and add to the word count if needed
+    for word in words:
+        if not should_filter_out_word(word):
+            stemmed_word = stemmer.stem(word)
+            
+            if stemmed_word in words_with_count:
+                words_with_count[stemmed_word] += 1
+            else:
+                words_with_count[stemmed_word] = 1
+            
+            if stemmed_word in overall_word_counts:
+                overall_word_counts[stemmed_word] += 1
+            else:
+                overall_word_counts[stemmed_word] = 1                
+            
     
-    return words
+    return words_with_count
 
-def get_body_words(reuter):
+def get_and_count_body_words(reuter,overall_word_counts):
     text_tag = reuter.find("text")
     body_text = text_tag.text
     
-    return get_word_list_and_reduce(body_text)
+    return get_reduced_words_with_their_counts(body_text,overall_word_counts)
 
-def count_words(word_list,dictionary):
-    for word in word_list:
-        if dictionary.get(word) == None:
-            dictionary[word] = 1
-        else:
-            dictionary[word] += 1
-
-"""
-def trim_words_based_upon_overall_frequency(data_matrix):
-    overall_max = -1 * sys.maxint
-    for column in data_matrix:
-        column_max = max(column)
-        if column_max > overall_max
-            overall_max = column_max
-    
-    for column in data_matrix:
-        max_column_value = max(column)
-        if max_column_value < 4 or max_column_value > 0.99 * overall_max:
-            del data_matrix.column
-"""
-
-def print_data(dictionary):
-    for key,value in dictionary.iteritems():
-        print "    "+key+": "+str(value)
-
-def add_to_class_label(reuter,dictionary):
+def get_class_label(reuter):
     reuter_text = reuter.find("text")
+    
     #get relevant fields from the REUTERS tag
     topics = reuter.topics
     places = reuter.places
     title = reuter_text.title
     
+    #init the class label dictionary
+    class_label = dict([])
+    class_label['topics'] = set()
+    class_label['places'] = set()
+    class_label['title'] = ""
+    
     #iterate and add words to label
     if topics != None:
         for child in topics.children:
-            dictionary['topics'].add(child.text)
+            class_label['topics'].add(child.text)
     if places != None:
         for child in places.children:
-            dictionary['places'].add(child.text)
+            class_label['places'].add(child.text)
     if title != None:
-        dictionary['title']=title.text
+        class_label['title']=title.text
+        
+    return class_label
+
+def term_frequency_filtering(data_matrix,overall_word_counts):
+    print "before term freq "+str(len(overall_word_counts))
+    for key in overall_word_counts.keys():
+        if overall_word_counts[key] < 20:
+            remove_word_from_data_matrix(key,data_matrix)
+            del overall_word_counts[key]
+    print "after term freq "+str(len(overall_word_counts))
+
+def remove_word_from_data_matrix(word,data_matrix):
+    data_matrix[0].remove(word)
+    for i in range(1,len(data_matrix)): #iterate over all the documents in the data matrix
+        if word in data_matrix[i][1]:
+            del data_matrix[i][1][word]
+
+"""
+The data matrix format is as follows:
+First row: The set of all interesting words in all the documents
+Subsequent rows: A list of length 2 representing a reuters document. The 0 index is the class label, and the 1 index is a dictionary of words ('word': word_count). If the word is not in that dictionary, then it is not found in the document.
+"""
+def get_data_matrix(directory_with_files): 
+    data_matrix = []
+    data_matrix.append( set() ) #first row is a set of all words across all documents
+    overall_word_counts = dict([]) #used to prevent having to reiterate over the data matrix a whole bunch for each term
     
-def init_transaction_data():
-    transaction_data = {'class_label': dict([]),'body':dict([])}  
-    #initialize the three fields of the class label
-    transaction_data['class_label']['topics'] = set()
-    transaction_data['class_label']['places'] = set()
-    transaction_data['class_label']['title'] = ""
-    return transaction_data
-    
-def get_transaction_data(directory_with_files): 
-    all_transaction_data = []
     for filename in os.listdir(directory_with_files):
         current_data_file = open(directory_with_files+"/"+filename, "r")
         sgml_tree = get_parsed_document_tree(current_data_file)
         
-        for reuter in sgml_tree.find_all("reuters"):    
-            transaction_data = init_transaction_data()
+        for reuter in sgml_tree.find_all("reuters"):       
+            class_label = get_class_label(reuter)
+            body_words_with_counts = get_and_count_body_words(reuter,overall_word_counts)
             
-            add_to_class_label(reuter,transaction_data['class_label'])
-            
-            #parse body into relevant word tokens
-            body_words = get_body_words(reuter)
-            count_words(body_words,transaction_data['body'])
-            
-            if len(transaction_data['body'].keys()) == 0:
-                transaction_data['body']['arbitrary_key_to_make_non_empty'] = 1
-            
-            all_transaction_data.append(transaction_data)
+            #add document to data matrix
+            data_matrix.append( [class_label,body_words_with_counts] )
+            data_matrix[0].update( body_words_with_counts.keys() )
         
         current_data_file.close()
-    return all_transaction_data
+    
+    term_frequency_filtering(data_matrix,overall_word_counts)
+    
+    return data_matrix
 
 def main():
     start_time = time.time()
-    all_transaction_data = get_transaction_data(os.getcwd()+"/../data_files")
+    data_matrix = get_data_matrix(os.getcwd()+"/../data_files")
     print("--- Program runs for %s seconds ---" % (time.time() - start_time))
     
-    for transaction_data in all_transaction_data:
+    """
+    i=0
+    for row in data_matrix:
         print ""
-        print_data(transaction_data['class_label'])
-        print_data(transaction_data['body'])
+        print row
+        i+=1
+        
+        if i>3:
+            break
+    """
     
 #calls the main() function
 if __name__ == "__main__":
