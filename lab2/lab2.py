8dadc7cdbcad2e2251c99efde3002075ea7d8562
jtronlabs <jtronlabs@gmail.com>
2015-09-28 19:31:51 -0400

    began working on different types of classifiers

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,8 +1,10 @@
 import numpy as np
+import time
 import json
 import sys
 import os
 import imp
+from sklearn import tree
 from sklearn.neighbors import NearestNeighbors
 
 lab1 = imp.load_source('lab1_script', os.getcwd()+"/../lab1/lab1_script.py")
@@ -23,14 +25,38 @@ array([[0, 1],
        [5, 4]]...)
 """
 
-from sklearn import tree
+def get_KNN_classifier(tfidf_data):
+    print ""
 
-"""
-X = [[0, 0], [1, 1]]
-Y = [0, 1]
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, Y)
-"""
+#returns a searchable decision tree and a list of words that was used to create training samples in order for that tree (this will be needed for the test data)
+def get_decision_tree(tfidf_data):
+    print "Building decision tree"
+    start_time = time.time()
+    
+    training_samples = list()
+    class_labels = list()
+    unique_words = list( get_unique_words_in_tfidf_data(tfidf_data) )
+    for row in tfidf_data:
+        #get the tfidf values of each unique word for every document. If word is not found in document then set value to 0.
+        row_training_samples = list()
+        for word in unique_words:
+            row_training_samples.append(row[1].get(word,0))
+        #add row data to overall data
+        training_samples.append(row_training_samples)
+        class_labels.append(row[0]['topics'])
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(training_samples, class_labels)
+    
+    print("Decision Tree creation ran for %s seconds. " % (time.time() - start_time))
+    return [clf,unique_words]
+
+def get_unique_words_in_tfidf_data(tfidf_data):
+    words = set()
+    for row in tfidf_data:
+        for key in row[1]:
+            words.add(key)
+    return words
+
 def load_feature_vectors_from_files():
     feature_vectors = list()
     tfidf_big_file = open(tfidf_big_dat_path, "r")
@@ -61,11 +87,17 @@ def run_lab1_feature_vector_extraction():
     return lab1.get_feature_vectors(original_data_files_directory)
 
 def main():
+    #get the feature vectors
     feature_vectors = get_feature_vectors()
     data_matrix = feature_vectors[0]
     tfidf_larger = feature_vectors[1]
     tfidf_smaller = feature_vectors[1]
     
+    #build classifiers for big tfidf data
+    tfidf_big_decision_tree_values = get_decision_tree(tfidf_larger)
+    
+    #build classifiers for small tfidf data
+    tfidf_small_decision_tree_values = get_decision_tree(tfidf_smaller)
 
 #calls the main() function
 if __name__ == "__main__":
